da418cecb4b49de371c8c1f5ae64791981b1fafa
c4nf3ng <root@macrodef.com>
2022-04-30 02:53:48 +0800

    fix

--- pac.js
+++
@@ -1,6 +0,0 @@
-function FindProxyForURL(url, host) {
-	if (shExpMatch(url, "*.mihoyo.com") || shExpMatch(url, "*.yuanshen.com")|| shExpMatch(url, "*.hoyoverse.com")){
-		return "PROXY 150.158.6.121:18080"
-	}
-	return "DIRECT"
-}
